334ebaafa9181ed42f68996d7092648ed014baff
Chenxia Liu <liuche@mozilla.com>
2018-07-13 14:21:29 -0700

    Issue #45 - Clean up Preference Compat usage.

--- app/src/main/java/org/mozilla/focus/SettingsActivity.kt
+++ app/src/main/java/org/mozilla/focus/SettingsActivity.kt
@@ -5,9 +5,17 @@
 package org.mozilla.focus
 
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.preference.PreferenceFragmentCompat
+import android.support.v7.preference.PreferenceScreen
 
+/**
+ * Settings activity with nested settings screens.
+ *
+ * We use AppCompatActivity and override some methods in PreferenceFragmentCompat in order to handle
+ * PreferenceScreen navigation.
+ */
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,12 +27,19 @@ class SettingsActivity : AppCompatActivity() {
         }
     }
 
-
-    class SettingsFragment : PreferenceFragmentCompat() {
-
+    class SettingsFragment : PreferenceFragmentCompat(), PreferenceFragmentCompat.OnPreferenceStartScreenCallback {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             addPreferencesFromResource(R.xml.settings)
         }
 
+        override fun onPreferenceStartScreen(preferenceFragmentCompat: PreferenceFragmentCompat, preferenceScreen: PreferenceScreen): Boolean {
+            (activity as AppCompatActivity).supportActionBar?.title = preferenceScreen.title
+            preferenceFragmentCompat.preferenceScreen = preferenceScreen
+            return true
+        }
+
+        override fun getCallbackFragment(): Fragment {
+            return this
+        }
     }
 }
