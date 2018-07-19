1bdf369b6957e7365c580cabe0e8fc577efa5537
Chenxia Liu <liuche@mozilla.com>
2018-07-18 17:54:54 -0700

    Issue #45 - Add Clear Data page
    This copies the behavior from the original settings clearing, but
    doesn't correctly clear the web history (although it does clear cookies
    and logs out from websites). Going back to MainActivity will still show
    the navigation buttons and you can visit history from there.

---
+++ app/src/main/java/org/mozilla/focus/widget/ClearDataFragment.kt
@@ -0,0 +1,42 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.focus.widget
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_cleardata.*
+import org.mozilla.focus.R
+import org.mozilla.focus.session.SessionManager
+import org.mozilla.focus.telemetry.TelemetryWrapper
+
+class ClearDataFragment: Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_cleardata, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        confirmButton.apply {
+            isSelected = true
+            setOnClickListener {
+                settingsWebView.cleanup()
+                SessionManager.getInstance().removeAllSessions()
+                TelemetryWrapper.clearDataEvent()
+                finishFragment()
+            }
+        }
+        cancelButton.setOnClickListener {
+            finishFragment()
+        }
+    }
+
+    private fun finishFragment() {
+        activity?.supportFragmentManager?.popBackStack()
+    }
+}
