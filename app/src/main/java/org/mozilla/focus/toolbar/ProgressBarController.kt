f376451f6b402a0634f32eb79b447ccca212684b
Michael Comella <michael.l.comella@gmail.com>
2018-08-02 16:40:56 -0700

    Closes #189: Don't update progress bar if not loading; other clean up.
    Instead of updating the progress bar directly from the progress bar
    value, we now gate it on isLoading state, which appears to fix the
    issues we experienced with the url bar flashing.

--- app/src/main/java/org/mozilla/focus/toolbar/ProgressBarController.kt
+++ app/src/main/java/org/mozilla/focus/toolbar/ProgressBarController.kt
@@ -4,33 +4,64 @@
 
 package org.mozilla.focus.toolbar
 
+import kotlinx.coroutines.experimental.CoroutineStart
+import kotlinx.coroutines.experimental.Job
 import kotlinx.coroutines.experimental.android.UI
 import kotlinx.coroutines.experimental.delay
 import kotlinx.coroutines.experimental.launch
+
+private const val MIN_UI_PROGRESS = 0
+private const val MAX_UI_PROGRESS = 100
 
 /** Controls the appearance of the progress bar. */
 class ProgressBarController(
         private val progressBar: UrlBoxProgressView
 ) {
+    private var isLoading = false
+    private var hideProgressBarJob: Job? = null
 
-    @Suppress("UNUSED_PARAMETER")
     fun onLoadingUpdate(isLoading: Boolean) {
+        this.isLoading = isLoading
+        if (isLoading) {
+            cancelPendingHideProgressBarJob()
+        } else {
+            hideProgressBar()
+        }
     }
 
-    fun onProgressUpdate(rawProgressValue: Int) {
-        // The max progress value is 99 (see comment in onProgress() in SessionCallbackProxy),
-        // thus we send 100 to the UrlBoxProgressView to complete its animation.
-        val adjustedProgressValue = if (rawProgressValue == 99) 100 else rawProgressValue
-        progressBar.progress = adjustedProgressValue
+    /**
+     * @param progressValue A progress value between 0 and 99.
+     */
+    fun onProgressUpdate(progressValue: Int) {
+        // On some pages, like cnn articles, this function may get called after the progress bar
+        // completes for the first time, causing the progress bar to look like it's loading again
+        // or blink on and off. To avoid this, we early return if we're not in the loading state.
+        if (!isLoading) {
+            return
+        }
 
-        // If the progress is 100% then we want to go back to 0 to hide the progress drawable
-        // again. However we want to show the full progress bar briefly so we wait 250ms before
-        // going back to 0.
-        if (adjustedProgressValue == 100) {
-            launch(UI) {
-                delay(250)
-                progressBar.progress = 0
-            }
+        progressBar.progress = progressValue
+    }
+
+    private fun cancelPendingHideProgressBarJob() {
+        hideProgressBarJob?.let {
+            it.cancel()
+            hideProgressBarJob = null
+        }
+    }
+
+    private fun hideProgressBar() {
+        if (progressBar.progress == MIN_UI_PROGRESS || // Already hidden.
+                hideProgressBarJob != null) { // Already hiding.
+            return
+        }
+
+        // The progress bar may hide even when set to an arbitrary percentage and it looks weird:
+        // we max out the progress bar briefly to make it look more natural.
+        progressBar.progress = MAX_UI_PROGRESS
+        hideProgressBarJob = launch(UI, CoroutineStart.UNDISPATCHED) {
+            delay(250)
+            progressBar.progress = MIN_UI_PROGRESS
         }
     }
 }
