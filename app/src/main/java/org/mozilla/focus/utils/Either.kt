9a1cce3c2bf0fceafcf5e18d85735441de85efc7
Michael Comella <michael.l.comella@gmail.com>
2018-09-21 15:41:47 -0700

    Closes #433 - review: Improve Either.fold readability.

--- app/src/main/java/org/mozilla/focus/utils/Either.kt
+++ app/src/main/java/org/mozilla/focus/utils/Either.kt
@@ -14,6 +14,21 @@ sealed class Either<out A, out B> {
     class Left<A>(val value: A) : Either<A, Nothing>()
     class Right<B>(val value: B) : Either<Nothing, B>()
 
+    /**
+     * Calls the [leftBranch] when the Either is of type Left, otherwise calls the [rightBranch],
+     * and returns a value of a type that both functions share.
+     *
+     * For example:
+     *   val eitherTextOrException: Either<String, InvalidParameterException> = getDisplayText(someString)
+     *   val displayText = result.fold(
+     *           { displayText -> displayText },
+     *           { exception -> "Error: exception occurred!" })
+     */
+    fun <C> fold(leftBranch: (A) -> C, rightBranch: (B) -> C): C = when (this) {
+        is Either.Left -> leftBranch(value)
+        is Either.Right -> rightBranch(value)
+    }
+
     val leftOrThrow: A
         get() = if (this is Either.Left) {
             value
@@ -27,13 +42,4 @@ sealed class Either<out A, out B> {
         } else {
             throw java.lang.IllegalStateException("Either: attempted to get right but value was left")
         }
-
-    /**
-     * Calls the [leftBranch] when the Either is of type Left, otherwise calls the [rightBranch],
-     * and returns a value of a type that both functions share.
-     */
-    fun <C> fold(leftBranch: (A) -> C, rightBranch: (B) -> C): C = when (this) {
-        is Either.Left -> leftBranch(value)
-        is Either.Right -> rightBranch(value)
-    }
 }
