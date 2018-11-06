61871e9c53b185a682f977cf8e51a69b22452db5
Michael Comella <michael.l.comella@gmail.com>
2018-11-06 15:30:01 -0800

    Issue #75: Add OnInterceptTouchEventFrameLayout.
    This will be necessary to observe touch events on the WebView's
    fullscreen view.

---
+++ app/src/main/java/org/mozilla/focus/widget/OnInterceptTouchEventFrameLayout.kt
@@ -0,0 +1,26 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.focus.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import android.widget.FrameLayout
+
+/**
+ * A [FrameLayout] that has an [onInterceptTouchEvent] equivalent to [View.setOnTouchListener].
+ */
+class OnInterceptTouchEventFrameLayout(
+        context: Context,
+        attrs: AttributeSet
+) : FrameLayout(context, attrs) {
+    var onInterceptTouchEventObserver: ((MotionEvent?) -> Unit)? = null
+
+    override fun onInterceptTouchEvent(motionEvent: MotionEvent?): Boolean {
+        motionEvent?.let { onInterceptTouchEventObserver?.invoke(it) }
+        return super.onInterceptTouchEvent(motionEvent)
+    }
+}
