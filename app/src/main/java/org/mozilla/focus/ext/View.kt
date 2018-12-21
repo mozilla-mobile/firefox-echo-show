94608662631f8a1ced1d723ecf58e417a0228a62
Michael Comella <michael.l.comella@gmail.com>
2018-12-20 19:26:37 -0800

    Issue #117: Add View.onGlobalLayoutOnce.

--- app/src/main/java/org/mozilla/focus/ext/View.kt
+++ app/src/main/java/org/mozilla/focus/ext/View.kt
@@ -6,6 +6,7 @@ package org.mozilla.focus.ext
 
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewTreeObserver
 
 val View.isVisible: Boolean get() = (visibility == View.VISIBLE)
 
@@ -14,4 +15,20 @@ inline fun View.updateLayoutParams(mutateLayoutParams: (ViewGroup.LayoutParams) 
     val layoutParams = this.layoutParams
     mutateLayoutParams(layoutParams)
     this.layoutParams = layoutParams // Calling setLayoutParams forces them to refresh internally.
+}
+
+/**
+ * Calls the given function on the next global layout call and never again.
+ *
+ * This may be replacable by ktx's View.doOnLayout.
+ */
+fun View.onGlobalLayoutOnce(onGlobalLayout: () -> Unit) {
+    viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+        override fun onGlobalLayout() {
+            // We must get the view tree observer again: a view tree observer reference can become inactive
+            // so we can't reuse it.
+            viewTreeObserver.removeOnGlobalLayoutListener(this)
+            onGlobalLayout()
+        }
+    })
 }
