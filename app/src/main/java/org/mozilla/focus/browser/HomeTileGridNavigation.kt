a7d70703a8b261aadf6e00c7e82af524b7a0945b
Michael Comella <michael.l.comella@gmail.com>
2019-01-23 17:24:07 -0800

    Closes #148: Remove usused preSetVisibilityListener.

--- app/src/main/java/org/mozilla/focus/browser/HomeTileGridNavigation.kt
+++ app/src/main/java/org/mozilla/focus/browser/HomeTileGridNavigation.kt
@@ -31,10 +31,7 @@ class HomeTileGridNavigation @JvmOverloads constructor(
     // since we init the tiles in View.init and Android is inflating the view for us,
     // thus we need to use Delegates.observable to update onTileLongClick.
     var homeTileLongClickListener: HomeTileLongClickListener? = null
-
     var onTileClicked: ((value: String) -> Unit)? = null
-    /** Called inside [setVisibility] right before super.setVisibility is called. */
-    var onPreSetVisibilityListener: ((isVisible: Boolean) -> Unit)? = null
 
     fun init(uiScope: CoroutineScope) {
         initTiles(uiScope)
@@ -66,7 +63,6 @@ class HomeTileGridNavigation @JvmOverloads constructor(
     }
 
     override fun setVisibility(visibility: Int) {
-        onPreSetVisibilityListener?.invoke(visibility == View.VISIBLE)
         super.setVisibility(visibility)
         scrollTo(0, 0)
     }
