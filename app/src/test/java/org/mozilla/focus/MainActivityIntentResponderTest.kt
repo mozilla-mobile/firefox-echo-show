ccfd8375074bca0305e001a1f1fbb8818fdc85b2
Michael Comella <michael.l.comella@gmail.com>
2019-02-28 15:53:06 -0800

    Issue #186: Fix tests ported to IntentResponder from Validator.

--- app/src/test/java/org/mozilla/focus/MainActivityIntentResponderTest.kt
+++ app/src/test/java/org/mozilla/focus/MainActivityIntentResponderTest.kt
@@ -4,18 +4,58 @@
 
 package org.mozilla.focus
 
-import org.junit.Ignore
+import android.content.Intent
+import android.content.Intent.ACTION_VIEW
+import android.content.Intent.FLAG_ACTIVITY_LAUNCHED_FROM_HISTORY
+import android.os.Bundle
+import mozilla.components.support.utils.SafeIntent
+import org.junit.Assert.fail
+import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito.mock
+import org.mozilla.focus.ext.toSafeIntent
+import org.mozilla.focus.ext.toUri
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
 
+private const val TEST_URL = "https://github.com/mozilla-mobile/focus-android"
+
+@RunWith(RobolectricTestRunner::class)
 class MainActivityIntentResponderTest {
 
-    @Ignore
-    @Test
-    fun `WHEN receiving a view intent with the launched from history flag THEN a null uri is returned`() {
+    private val context get() = RuntimeEnvironment.application
+
+    private lateinit var responder: MainActivityIntentResponder
+    private lateinit var loadUrlFun: (String) -> Unit
+
+    private lateinit var intent: SafeIntent
+
+    @Before
+    fun setUp() {
+        loadUrlFun = {}
+        responder = MainActivityIntentResponder { loadUrlFun(it) }
+
+        intent = Intent(ACTION_VIEW, TEST_URL.toUri()).toSafeIntent()
     }
 
-    @Ignore
     @Test
-    fun `WHEN the activity is restoring state THEN no url is returned`() {
+    fun `WHEN receiving a view intent in onCreate with the launched from history flag THEN a url is not loaded`() {
+        val intent = intent.unsafe.apply {
+            addFlags(FLAG_ACTIVITY_LAUNCHED_FROM_HISTORY)
+        }.toSafeIntent()
+
+        setLoadUrlIsFail()
+        responder.onCreate(context, null, intent)
+    }
+
+    @Test
+    fun `WHEN the activity is restoring state THEN no url is loaded`() {
+        setLoadUrlIsFail()
+        responder.onCreate(context, mock(Bundle::class.java), intent)
+    }
+
+    private fun setLoadUrlIsFail() {
+        loadUrlFun = { fail("Did not expect loadUrl to get called") }
     }
 }
